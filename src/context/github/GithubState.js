f24f6a12ee69f00898c49168d7b9ac97c6ac25c8
Lee Colsant <lee.colsant@gmail.com>
2021-06-04 22:51:33 -0700

    moving user state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -36,6 +36,27 @@ const GithubState = (props) => {
     });
   };
 
+  // Get single Github user
+  const getUser = async (username) => {
+    setLoading();
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=$
+          {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+          {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: GET_USER,
+      payload: res.data,
+    });
+  };
+
+  //Get Repos
+
+  //Clear users from state
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
   //Set Loading
   const setLoading = () => dispatch({ type: SET_LOADING });
 
@@ -47,8 +68,8 @@ const GithubState = (props) => {
         repos: state.repos,
         loading: state.loading,
         searchUsers,
-        // clearUsers,
-        // getUser,
+        clearUsers,
+        getUser,
         // getUserRepos,
       }}
     >
